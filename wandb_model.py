a1e2f74785c79b592ec19213bec85a110b7ba26c
Alexander Davies <alexander_davies@college.harvard.edu>
2022-02-02 22:17:53 -0500

    remove stage check from setup

--- wandb_model.py
+++ wandb_model.py
@@ -36,15 +36,13 @@ class CIFAR10DataModule(pl.LightningDataModule):
 
     def setup(self, stage: str = None) -> None:
         # Assign train/val datasets for use in dataloaders
-        if stage == 'fit' or stage is None:
-            cifar_full = CIFAR10(self.data_dir, train=True,
-                                 transform=self.transform)
-            self.cifar_train, self.cifar_val = random_split(
-                cifar_full, [45000, 5000])
+        cifar_full = CIFAR10(self.data_dir, train=True,
+                             transform=self.transform)
+        self.cifar_train, self.cifar_val = random_split(
+            cifar_full, [45000, 5000])
 
-        if stage == 'test' or stage is None:
-            self.cifar_test = CIFAR10(
-                self.data_dir, train=False, transform=self.transform)
+        self.cifar_test = CIFAR10(
+            self.data_dir, train=False, transform=self.transform)
 
     def train_dataloader(self):
         return DataLoader(self.cifar_train, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
